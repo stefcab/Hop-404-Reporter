a0722081d307d08d7986f76d70f639a46a874c00
Louis DEKEISTER <louis@hopstudios.com>
2015-12-16 17:01:06 -0800

    Use new EE3 style for add email page

--- ee3x/system/user/addons/hop_404_reporter/views/add_email.php
+++ ee3x/system/user/addons/hop_404_reporter/views/add_email.php
@@ -1,48 +1,48 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<h3><?=lang('email_notif_page_title')?></h3>
 
-<?=form_open($action_url, '', $form_hidden)?>
 
-<table class="mainTable padTable" border="0" cellspacing="0" cellpadding="0">
-	<thead>
-		<tr class="even">
-			<th style="width:50%;" class=""><?=lang('preference')?></th><th><?=lang('setting')?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr class="<?php echo alternator('even', 'odd');?>">
-			<td>
-				<strong><label for="email_address"><?=lang('email_notif_email_label')?></label></strong><div class="subtext"><?=lang('email_notif_email_desc')?></div>
-			</td>
-			<td>
-				<input type="text" name="email_address" checked="checked" id="email_address" <?php if (isset($form_value_email)) echo 'value="'.$form_value_email.'"';?>>&nbsp;
-				<?php if (isset($form_error_email)) echo $form_error_email;?>
-			</td>
-		</tr>
-		<tr class="<?php echo alternator('even', 'odd');?>">
-			<td>
-				<strong><label for="url_filter"><?=lang('email_notif_url_label')?></label></strong><div class="subtext"><?=lang('email_notif_url_desc')?></div>
-			</td>
-			<td>
-				<input type="text" name="url_filter" checked="checked" id="url_filter" <?php if (isset($form_value_url_filter)) echo 'value="'.$form_value_url_filter.'"';?>>&nbsp;
-				<?php if (isset($form_error_url_filter)) echo $form_error_url_filter;?>
-			</td>
-		</tr>
-		<tr class="<?php echo alternator('even', 'odd');?>">
-			<td>
-				<strong><label for="notification_interval"><?=lang('email_notif_interval_label')?></label></strong>
-				<div class="subtext"><?=lang('email_notif_interval_desc')?></div>
-			</td>
-			<td>
+<div class="box">
+	<div class="tbl-ctrls">
+		<h1><?=lang('email_notif_page_title')?></h1>
+
+		<?=form_open($action_url, '', $form_hidden)?>
+		<fieldset class="col-group required <?php if (isset($form_error_email)) echo 'invalid';?>">
+			<div class="setting-txt col w-8">
+				<h3><?=lang('email_notif_email_label')?></h3>
+				<em><?=lang('email_notif_email_desc')?></em>
+			</div>
+			<div class="setting-field col w-8 last">
+				<input type="text" name="email_address" checked="checked" id="email_address" <?php if (isset($form_value_email)) echo 'value="'.$form_value_email.'"';?>>
+				<?php if (isset($form_error_email)) echo '<em>'.$form_error_email.'</em>';?>
+			</div>
+		</fieldset>
+		<fieldset class="col-group required <?php if (isset($form_error_url_filter)) echo 'invalid';?>">
+			<div class="setting-txt col w-8">
+				<h3><?=lang('email_notif_url_label')?></h3>
+				<em><?=lang('email_notif_url_desc')?></em>
+			</div>
+			<div class="setting-field col w-8 last">
+				<input type="text" name="url_filter" checked="checked" id="email_address" <?php if (isset($form_value_url_filter)) echo 'value="'.$form_value_url_filter.'"';?>>
+				<?php if (isset($form_error_url_filter)) echo '<em>'.$form_error_url_filter.'</em>';?>
+			</div>
+		</fieldset>
+		<fieldset class="col-group required <?php if (isset($form_error_interval)) echo 'invalid';?>">
+			<div class="setting-txt col w-8">
+				<h3><?=lang('email_notif_interval_label')?></h3>
+				<em><?=lang('email_notif_interval_desc')?></em>
+			</div>
+			<div class="setting-field col w-8 last">
 				<select name="notification_interval" id="notification_interval">
 					<option value="once"><?=lang('email_notif_interval_once')?></option>
 					<option value="always"><?=lang('email_notif_interval_always')?></option>
 				</select>&nbsp;
-				<?php if (isset($form_error_interval)) echo $form_error_interval;?>
-			</td>
-		</tr>
-	</tbody>
-</table>
+				<?php if (isset($form_error_interval)) echo '<em>'.$form_error_interval.'</em>';?>
+			</div>
+		</fieldset>
 
-<?=form_submit(array('name' => 'submit', 'value' => lang('email_notif_submit'), 'class' => 'submit'))?>
-<?=form_close()?>
+		<fieldset class="form-ctrls">
+			<?=form_submit(array('name' => 'submit', 'value' => lang('email_notif_submit'), 'class' => 'btn submit'))?>
+		</fieldset>
+		<?=form_close()?>
+	</div>
+</div>
